02ecdc90208084ed9970e86f06b20d305d410f9d
Dave MacFarlane <driusan@gmail.com>
2016-07-02 11:04:36 -0400

    Enabled horizontal scrolling with trackpad on systems
    This adds support for scrolling left or right with the trackpad (or anything
    which generates ButtonWheelLeft or ButtonWheelRight events) on any shiny
    driver which has support for it.

--- viewer/mouse.go
+++ viewer/mouse.go
@@ -10,32 +10,51 @@ import (
 // Handles a mousewheel event. Returns true if the event did something that
 // should result in a repainting of the screen.
 func (v *Viewport) HandleMouseWheel(e mouse.Event, buff *demodel.CharBuffer, wSize image.Point) bool {
-	var scrollY int
-	imgSize := v.Renderer.Bounds(buff)
+	var scrollAmt int
+	var scrollAxis *int
 	lineSize := renderer.MonoFontFace.Metrics().Height.Ceil()
 	switch e.Button {
 	case mouse.ButtonWheelUp:
 		if v.Location.Y == 0 {
 			return false
 		}
-		scrollY = -lineSize
+		scrollAxis = &v.Location.Y
+		scrollAmt = -lineSize
 	case mouse.ButtonWheelDown:
 		if v.Location.Y == wSize.Y-50 {
 			return false
 		}
-		scrollY = lineSize
+		scrollAxis = &v.Location.Y
+		scrollAmt = lineSize
+	case mouse.ButtonWheelLeft:
+		scrollAxis = &v.Location.X
+		scrollAmt = -lineSize
+	case mouse.ButtonWheelRight:
+		scrollAxis = &v.Location.X
+		scrollAmt = lineSize
+	default:
+		scrollAmt = 0
+		scrollAxis = nil
 	}
 
-	if scrollY == 0 {
+	if scrollAmt == 0 || scrollAxis == nil {
 		// This should be unreachable.
 		return false
 	}
 
-	v.Location.Y += scrollY
-	if v.Location.Y < 0 || imgSize.Size().Y < wSize.Y {
+	*scrollAxis += scrollAmt
+	imgSize := v.Renderer.Bounds(buff).Size()
+	if v.Location.Y < 0 || imgSize.Y < wSize.Y {
 		v.Location.Y = 0
-	} else if v.Location.Y+wSize.Y > imgSize.Max.Y+50 {
-		v.Location.Y = imgSize.Max.Y - wSize.Y + 50
+	} else if v.Location.Y+wSize.Y > imgSize.Y+50 {
+		v.Location.Y = imgSize.Y - wSize.Y + 50
 	}
+
+	if v.Location.X < 0 || imgSize.X < wSize.X {
+		v.Location.X = 0
+	} else if v.Location.X+wSize.X > imgSize.X+50 {
+		v.Location.X = imgSize.X - wSize.X + 50
+	}
+
 	return true
 }
