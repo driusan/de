229741960611a0c51a7159fa529101f211be71c9
Dave MacFarlane <driusan@gmail.com>
2017-01-22 19:56:33 -0500

    Started improving shell command
    Shell has been sadly neglected and needs some love.
    
    This is a start.

--- plugins/shell/shell.go
+++ plugins/shell/shell.go
@@ -1,14 +1,17 @@
 package shell
 
 import (
-	"bytes"
+	//"bytes"
+	"log"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
 	"runtime"
-	"sync"
-	"time"
+	//"sync"
+	"syscall"
+
+	"github.com/pkg/term/termios"
 
 	"github.com/driusan/de/actions"
 	"github.com/driusan/de/demodel"
@@ -16,31 +19,7 @@ import (
 	"golang.org/x/mobile/event/key"
 )
 
-// Create a thread safe wrapper around bytes.Buffer, so that our go routine can read
-// from it despite the fact that the shell might be writing to it.
-type TSBuffer struct {
-	b bytes.Buffer
-	m sync.Mutex
-}
-
-func (b *TSBuffer) Read(p []byte) (n int, err error) {
-	b.m.Lock()
-	defer b.m.Unlock()
-	return b.b.Read(p)
-}
-func (b *TSBuffer) Write(p []byte) (n int, err error) {
-	b.m.Lock()
-	defer b.m.Unlock()
-	return b.b.Write(p)
-}
-func (b *TSBuffer) String() string {
-	b.m.Lock()
-	defer b.m.Unlock()
-	return b.b.String()
-}
-
 type shellKbmap struct {
-	//runeChan chan rune
 	stdin io.Writer
 }
 
@@ -48,12 +27,14 @@ type shellKbmap struct {
 // normal mode), arrow keys (scroll the viewport)
 func (s shellKbmap) HandleKey(e key.Event, buff *demodel.CharBuffer, v demodel.Viewport) (demodel.Map, demodel.ScrollDirection, error) {
 	switch e.Code {
-	case key.CodeEscape:
-		// TODO: Quit the shell as the documentation claims happens.
-		if e.Direction != key.DirPress {
-			//return kbmap.NormalMode, demodel.DirectionNone, nil
-		}
-		return s, demodel.DirectionDown, nil
+	/*
+		case key.CodeEscape:
+			// TODO: Quit the shell as the documentation claims happens.
+			if e.Direction != key.DirPress {
+				//return kbmap.NormalMode, demodel.DirectionNone, nil
+			}
+			return s, demodel.DirectionDown, nil
+	*/
 	// Still honour the viewport manipulation keys
 	case key.CodeRightArrow:
 		// Arrow keys indicate their scroll direction via the error return value,
@@ -79,9 +60,10 @@ func (s shellKbmap) HandleKey(e key.Event, buff *demodel.CharBuffer, v demodel.V
 		}
 		return s, demodel.DirectionNone, nil
 	// Special cases for control characters.
+
 	case key.CodeTab:
-		if e.Direction != key.DirPress {
-			buff.Buffer = append(buff.Buffer, '\t')
+		if e.Direction != key.DirRelease {
+			//buff.Buffer = append(buff.Buffer, '\t')
 			fmt.Fprintf(s.stdin, "%c", '\t')
 			buff.Dot.End = uint(len(buff.Buffer)) - 1
 			buff.Dot.Start = buff.Dot.End
@@ -89,16 +71,16 @@ func (s shellKbmap) HandleKey(e key.Event, buff *demodel.CharBuffer, v demodel.V
 		return s, demodel.DirectionDown, nil
 		//	fmt.Printf("Pressed key %s. Rune is %x", e, e.Rune
 	case key.CodeDeleteBackspace:
-		if e.Direction != key.DirPress {
-			buff.Buffer = buff.Buffer[:len(buff.Buffer)-1] //append(buff.Buffer, "\t")
+		if e.Direction != key.DirRelease {
+			//buff.Buffer = buff.Buffer[:len(buff.Buffer)-1] //append(buff.Buffer, "\t")
 			fmt.Fprintf(s.stdin, "%c", '\b')
 			buff.Dot.End = uint(len(buff.Buffer)) - 1
 			buff.Dot.Start = buff.Dot.End
 		}
 		return s, demodel.DirectionDown, nil
 	case key.CodeReturnEnter:
-		if e.Direction != key.DirPress {
-			buff.Buffer = append(buff.Buffer, '\n')
+		if e.Direction != key.DirRelease {
+			//buff.Buffer = append(buff.Buffer, '\n')
 			fmt.Fprintf(s.stdin, "%c", '\n')
 			buff.Dot.End = uint(len(buff.Buffer)) - 1
 			buff.Dot.Start = buff.Dot.End
@@ -106,30 +88,21 @@ func (s shellKbmap) HandleKey(e key.Event, buff *demodel.CharBuffer, v demodel.V
 		}
 		return s, demodel.DirectionDown, nil
 	default:
-		if e.Direction != key.DirPress && e.Rune > 0 {
-			// send the rune to the buffer and to the shell
-			//rbytes := make([]byte, 4)
-			//n := utf8.EncodeRune(rbytes, e.Rune)
-			//	fmt.Printf("Sent to stdin: %c %d", e.Rune, e.Rune)
-			// bash and zsh echo the character typed back when invoked with $SHELL -i
-			// and it's not a tty.
-			// dash doesn't.
-			// Don't append the rune to the buffer, because odds are high it'll
-			// get echoed back, though there's no way to know for sure.
-			//buff.Buffer = append(buff.Buffer, rbytes[:n]...)
-
+		if e.Direction != key.DirRelease && e.Rune > 0 {
+			// Hacks for some control codes where it's
+			// not set properly in e.Rune.
+			if e.Modifiers&key.ModControl != 0 {
+				switch e.Rune {
+				case 'd':
+					e.Rune = 4
+				case 'z':
+					e.Rune = 26
+				}
+			}
 			fmt.Fprintf(s.stdin, "%c", e.Rune)
 			buff.Dot.End = uint(len(buff.Buffer)) - 1
 			buff.Dot.Start = buff.Dot.End
 
-		} else {
-			/*
-					for debugging only. This otherwise triggers errors on things like
-					the user pressing a control key.
-
-				if e.Rune <= 0 {
-					fmt.Printf("Invalid rune %d from %s\n", e.Rune, e)
-				}*/
 		}
 	}
 	return s, demodel.DirectionDown, nil
@@ -154,36 +127,45 @@ func Shell(args string, buff *demodel.CharBuffer, viewport demodel.Viewport) {
 	}
 
 	c := exec.Command(shell, "-i")
-	//c := exec.Command("sh")
-	stdin, _ := c.StdinPipe()
-	kbMap := &shellKbmap{stdin}
-	viewport.LockKeyboardMode(kbMap)
 
 	buff.Filename = ""
 
 	go func() {
-		var stdOut TSBuffer //bytes.Buffer
-		//	var stdErr bytes.Buffer
-		c.Stdout = &stdOut
-		c.Stderr = &stdOut
-		//stdout, _ := c.StdoutPipe()
-		//stderr, _ := c.StderrPipe()
+		masterTTY, slaveTTY, err := termios.Pty()
+		// _, slaveTTY, err := termios.Pty()
+		kbMap := &shellKbmap{masterTTY}
+		viewport.LockKeyboardMode(kbMap)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		procs := syscall.SysProcAttr{
+			Setsid: true,
+		}
+		c.SysProcAttr = &procs
+		c.Stdout = slaveTTY
+		c.Stderr = slaveTTY
+		c.Stdin = slaveTTY
 		c.Start()
 
 		// buffer to read lines into. Allocate this out of the loop to go
 		// easier on the GC.
 		termline := make([]byte, 1024)
-		for {
-			viewport.SetRenderer(&TerminalRenderer{})
+		viewport.SetRenderer(&TerminalRenderer{})
+
+		var quit bool
+		go func() {
+			c.Wait()
+			quit = true
+		}()
+		for !quit {
 			if buff.Filename != "" {
 				// The user must have clicked on a filename and opened it.
 				// Stop the Shell.
-				stdin.Close()
 				break
 			}
 
-			//fmt.Printf("reading from stdout\n")
-			n, _ := stdOut.Read(termline)
+			n, _ := masterTTY.Read(termline)
 
 			if n > 0 {
 				buff.Buffer = append(buff.Buffer, termline[:n]...)
@@ -196,14 +178,12 @@ func Shell(args string, buff *demodel.CharBuffer, viewport demodel.Viewport) {
 				buff.Dot.End = uint(len(buff.Buffer)) - 1
 				buff.Dot.Start = buff.Dot.End
 				//fmt.Printf("Requesting rerender\n")
-				viewport.Rerender()
-			} else {
-				time.Sleep(100 * time.Millisecond)
+				//viewport.Rerender()
 			}
 		}
-		c.Wait()
-		fmt.Fprintf(os.Stderr, "Shell exited\n")
 		viewport.UnlockKeyboardMode(kbMap)
 		viewport.SetKeyboardMode(kbmap.NormalMode)
+		viewport.Rerender()
+
 	}()
 }
